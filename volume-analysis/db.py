d93e3a75ccf15871c53d818759741406bc3483a2
zakhtarch <zakhtar@keeperdao.com>
2022-06-29 15:58:49 -0500

    Staging work

--- volume-analysis/db.py
+++ volume-analysis/db.py
@@ -10,7 +10,7 @@ import matplotlib
 matplotlib.use( 'tkagg' )
 
 
-DB_LOGIN = ""
+DB_LOGIN = "postgresql+psycopg2://readonly:0Llz$FU10&$znENe@staging-rook-api-db.cgyy5qxl0fmp.us-east-2.rds.amazonaws.com/staging"
 
 
 # Create an engine instance
@@ -46,62 +46,6 @@ dailyVolume = []
 
 day = launchDate
 timestamp = datetime.datetime.timestamp(day)
-
-print(f"Timestamp: {timestamp}")
-
-while timestamp < datetime.datetime.timestamp(datetime.datetime.now()):
-
-    dailyFills = volumeUSD[volumeUSD["timestamp"] > timestamp]
-
-    dailyFills = dailyFills[dailyFills["timestamp"] <= datetime.datetime.timestamp(day + datetime.timedelta(days=1))]
-
-    dailyVolume.append(dailyFills["makerTokenFilledAmountUSD"].sum())
-    datetimes.append(day + datetime.timedelta(days=1))
-
-    usdVoume = dailyFills["makerTokenFilledAmountUSD"].sum()
-
-    # print(f"volume from {day} to {day + datetime.timedelta(days=1)}: {usdVoume}")
-
-    day = day + datetime.timedelta(days=1)
-    timestamp = datetime.datetime.timestamp(day)
-
-dailyVolumeUSD = pds.DataFrame({"Date": datetimes, "volumeUSD": dailyVolume})
-dailyVolumeUSD.set_index("Date", inplace=True)
-revenue = pds.read_csv(
-    "Daily_Supply-Side_Revenue_Vs._Protocol_Revenue_In_The_Past_180_Days._2022-06-29.csv", index_col="Date"
-)
-
-dailyVolumeUSD.index = pds.to_datetime(dailyVolumeUSD.index)
-revenue.index = pds.to_datetime(revenue.index)
-
-total_volume_USD = volumeUSD["makerTokenFilledAmountUSD"].sum()
-total_supply_revenue = revenue["Protocol Revenue ($)"].sum()
-total_protocol_revenue = revenue["Treasury Revenue ($)"].sum()
-
-revenue = revenue.resample('3D').sum()
-dailyVolumeUSD = dailyVolumeUSD.resample('3D').sum()
-revenue = revenue.cumsum()
-dailyVolumeUSD = dailyVolumeUSD.cumsum()
-
-fmt_str = "%d-%m"
-dailyVolumeUSD.index = dailyVolumeUSD.index.strftime(fmt_str)
-revenue.index = revenue.index.strftime(fmt_str)
-
-ax1 = dailyVolumeUSD.plot(kind="bar")
-# plt.ylim((10^6, 300*10^6))
-ax2 = ax1.twinx()
-revenue.plot(kind="bar", ax=ax2, color=["#ff7f0e", "#2ca02c"])
-
-ax2.set_ylim(0, 300000)
-# ax2.set_ylim(0, 5000)
-
-plt.show()
-
-print(total_protocol_revenue)
-print(total_supply_revenue)
-print(total_volume_USD)
-print(f"Protocol revenue to volume ratio: {total_supply_revenue / total_volume_USD}")
-print(f"Treasury revenue to volume ratio: {total_protocol_revenue / total_volume_USD}")
 
 
 ### ROOK BID STUFF
@@ -145,5 +89,86 @@ bid_volume_data["correctedRookBidUSD"] = bid_volume_data["rookPrice"].mul(
 
 # USE THIS ONE FOR PLOTTING. correctedRookBidUSD is the value you want
 bid_volume_data["Date"] = [datetime.datetime.fromtimestamp(timestamp) for timestamp in bid_volume_data["timestamp"]]
+
+
+# print(f"Timestamp: {timestamp}")
+
+while timestamp < datetime.datetime.timestamp(datetime.datetime.now()):
+
+    dailyFills = volumeUSD[volumeUSD["timestamp"] > timestamp]
+
+    dailyFills = dailyFills[dailyFills["timestamp"] <= datetime.datetime.timestamp(day + datetime.timedelta(days=1))]
+
+    dailyVolume.append(dailyFills["makerTokenFilledAmountUSD"].sum())
+    datetimes.append(day + datetime.timedelta(days=1))
+
+    usdVoume = dailyFills["makerTokenFilledAmountUSD"].sum()
+
+    # print(f"volume from {day} to {day + datetime.timedelta(days=1)}: {usdVoume}")
+
+    day = day + datetime.timedelta(days=1)
+    timestamp = datetime.datetime.timestamp(day)
+
+bid_volume_data.set_index('Date', inplace=True)
+dailyVolumeUSD = pds.DataFrame({"Date": datetimes, "volumeUSD": dailyVolume})
+dailyVolumeUSD.set_index("Date", inplace=True)
+revenue = pds.read_csv(
+    "Daily_Supply-Side_Revenue_Vs._Protocol_Revenue_In_The_Past_180_Days._2022-06-29.csv", index_col="Date"
+)
+
+dailyVolumeUSD.index = pds.to_datetime(dailyVolumeUSD.index)
+revenue.index = pds.to_datetime(revenue.index)
+bid_volume_data.index = pds.to_datetime(bid_volume_data.index)
+
+total_volume_USD = volumeUSD["makerTokenFilledAmountUSD"].sum()
+total_supply_revenue = revenue["Protocol Revenue ($)"].sum()
+total_protocol_revenue = revenue["Treasury Revenue ($)"].sum()
+
+revenue = revenue.resample('3D').sum()
+dailyVolumeUSD = dailyVolumeUSD.resample('3D').sum()
+bid_volume_data = bid_volume_data.resample('3D').sum()
+revenue = revenue.cumsum()
+dailyVolumeUSD = dailyVolumeUSD.cumsum()
+bid_volume_data = bid_volume_data.cumsum()
+
+fmt_str = "%d-%m"
+dailyVolumeUSD.index = dailyVolumeUSD.index.strftime(fmt_str)
+revenue.index = revenue.index.strftime(fmt_str)
+bid_volume_data.index = bid_volume_data.index.strftime(fmt_str)
+
+# bid_volume_data = bid_volume_data.loc[:, ['correctedRookBidUSD', 'makerTokenFilledAmountUSD']]
+
+bid_volume_data['income_ratio'] = bid_volume_data['correctedRookBidUSD']/bid_volume_data['makerTokenFilledAmountUSD']
+
+fig = plt.figure()
+
+ax1 = fig.add_subplot(111)
+ax2 = ax1.twinx()
+
+bid_volume_data.plot(kind='line', ax=ax1, y='income_ratio')
+bid_volume_data.plot(kind='bar', ax=ax2, y=['correctedRookBidUSD', 'makerTokenFilledAmountUSD'], color=['blue', 'red'])
+# # revenue.plot(kind="bar", ax=ax2, color=["#ff7f0e", "#2ca02c"])
+# bid_volume_data.correctedRookBidUSD.plot(kind="bar", ax=ax2, #color=["#ff7f0e", "#2ca02c"])
+
+ax1.legend(loc='upper left')
+ax2.legend(loc='upper right')
+
+ax1.set_ylabel('Income Ratio')
+ax2.set_ylabel('Volume')
+
+# plt.ylim((10^6, 300*10^6))
+ax1.set_ylim(0, .002)
+# ax2.set_ylim(0, 300000)
+
+plt.show()
+
+print(total_protocol_revenue)
+print(total_supply_revenue)
+print(total_volume_USD)
+print(f"Protocol revenue to volume ratio: {total_supply_revenue / total_volume_USD}")
+print(f"Treasury revenue to volume ratio: {total_protocol_revenue / total_volume_USD}")
+
+print(bid_volume_data)
+
 
 dbConnection.close()
